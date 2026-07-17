e392d9943065ad3a54552bfd80a159ccde7c0762
trykpa1755 <trykpa1755@wp.pl>
2026-07-17 18:21:07 +0200

    LEkcja28

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform closePosition;
+    public Transform openPosition;
+    public Transform door;
+
+    public bool open = false;
+    float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door.position = closePosition.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open && Vector3.Distance(door.position, openPosition.position) > 0.001f)
+        {
+            door.position = Vector3.MoveTowards(door.position, openPosition.position,
+                Time.deltaTime * speed);
+        }
+    }
+
+    public void Open()
+    {
+        open = true;
+    }
+
+
+}
